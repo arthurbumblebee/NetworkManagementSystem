399eed94d7e1182e2d92f9514d672edae4f24f5b
arthurbumblebee <amakumbi@colby.edu>
2017-07-25 15:55:44 -0400

    testing

--- node.js
+++ node.js
@@ -39,6 +39,7 @@ http.createServer(function(request, response) {
     var queryaction = dot.remove('queryAction', query);
     console.log("querying ", queryaction);
 
+
     if (queryaction == "showLocation") {
         var sqlshowLocation = "SELECT * FROM robotLocation WHERE robotID = ?";
         var locateItem = dot.remove("robotid", query);
